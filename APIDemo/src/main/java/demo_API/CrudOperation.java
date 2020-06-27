81857ef3cef50dc542eea49586762874e343409b
Atul <akm.elex0007@gmail.com>
2020-06-27 20:41:02 +0530

    Crud operation updated

--- APIDemo/src/main/java/demo_API/CrudOperation.java
+++ APIDemo/src/main/java/demo_API/CrudOperation.java
@@ -2,15 +2,17 @@ package demo_API;
 
 import static org.hamcrest.Matchers.equalTo;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import junit.framework.Assert;
+
 import udemyFiles.Payloads;
 import udemyFiles.ReuseableMethods;
 
-public class Postman {
+public class CrudOperation {
+
 	@Test
 	public void Testing() {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
@@ -24,24 +26,21 @@ public class Postman {
 
 		String placeId = js.getString("place_id");
 		System.out.println(placeId);
-		
-		
-		String newAddress="335,MRA HOSTEL";
+
+		String newAddress = "335,MRA HOSTEL";
 		// Update place~~~~PUT Method~~~~
 		RestAssured.given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body(Payloads.updatePlace(placeId))
-				.when().put("maps/api/place/update/json")
-				.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
-				
-		//GET Place~~~~GET Method~~~~~
-		String getPlaceResponse=RestAssured.given().log().all().queryParam("key", "qaclick123")
-		.queryParam("place_id", placeId)
-		.when().get("maps/api/place/get/json")
-		.then().assertThat().log().all().statusCode(200).extract().asString();
-		JsonPath js1=ReuseableMethods.rawToJson(getPlaceResponse);
-		String actualAddress=js1.getString("address");
-		System.out.println("Actual Address :"+actualAddress);
+				.body(Payloads.updatePlace(placeId)).when().put("maps/api/place/update/json").then().assertThat().log()
+				.all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+		// GET Place~~~~GET Method~~~~~
+		String getPlaceResponse = RestAssured.given().log().all().queryParam("key", "qaclick123")
+				.queryParam("place_id", placeId).when().get("maps/api/place/get/json").then().assertThat().log().all()
+				.statusCode(200).extract().asString();
+		JsonPath js1 = ReuseableMethods.rawToJson(getPlaceResponse);
+		String actualAddress = js1.getString("address");
+		System.out.println("Actual Address :" + actualAddress);
 		Assert.assertEquals(actualAddress, newAddress);
-		
+
 	}
 }
